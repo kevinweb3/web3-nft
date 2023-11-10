9bad343e5fd5a558ca30d7f5dbf1e212c50549b0
kevinweb3 <kevinzhw@outlook.com>
2023-11-10 20:38:36 +0800

    commit

---
+++ contracts/MyNFT.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+    uint256 private _nextTokenId;
+
+    constructor(
+        address initialOwner
+    ) ERC721("MyNFT", "NFT") Ownable(initialOwner) {}
+
+    function mintNFT(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // 给NFT定价
+    function mintToken(
+        address to,
+        uint256 tokenId,
+        string memory uri
+    ) public payable virtual {
+        require(msg.value >= 10, "Not enough ETH sent; check price!");
+
+        mint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    ) internal override(ERC721, ERC721Enumerable) returns (address) {
+        return super._update(to, tokenId, auth);
+    }
+
+    function _increaseBalance(
+        address account,
+        uint128 value
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._increaseBalance(account, value);
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    )
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
---
+++ scripts/AwesomeGameDeploy.ts
@@ -0,0 +1,48 @@
+import "@nomicfoundation/hardhat-toolbox";
+import { ethers } from "hardhat";
+import fs from "fs";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  const address = deployer.address;
+  console.log("获取部署合约账户地址：", address);
+
+  const contractFactory = await ethers.getContractFactory("AwesomeGame");
+  const deployContract = await contractFactory.deploy(address);
+  await deployContract.waitForDeployment();
+
+  const contractAddress = await deployContract.getAddress();
+  console.log("合约部署地址：", contractAddress);
+
+  // 获取部署合约信息保存到本地文件，供前端合约交互使用
+  const contractInfos = {
+    contractName: await deployContract.name(),
+    contractSymbol: await deployContract.symbol(),
+    contractTotalSupply: String(await deployContract.totalSupply()),
+    contractBalance: String(await deployContract.balanceOf(address)),
+    contractAddress,
+  };
+
+  // 将合约地址和部署账户信息生成json文件传给前端
+  saveFrontendFiles("MyNFT", contractInfos);
+}
+
+function saveFrontendFiles(
+  name: string,
+  _contractInfos: { contractName: string; contractSymbol: string; contractTotalSupply: string; contractBalance: string; contractAddress: string}
+) {
+  const contractsAddressDir = __dirname + "/../contractInfo";
+
+  if (!fs.existsSync(contractsAddressDir)) {
+    fs.mkdirSync(contractsAddressDir);
+  }
+
+  const path = contractsAddressDir + '/' + name + ".json";
+
+  fs.writeFileSync(path, JSON.stringify(_contractInfos, null, 2));
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
